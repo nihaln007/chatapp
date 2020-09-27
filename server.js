f79bc5c274779cd35bbc7dfa8293bf246d1ef69c
Nihal <nihalnyt@gmail.com>
2020-09-27 13:13:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.use(express.static(__dirname + "/public"));
+//socket
+
+io.on("connection", (socket) => {
+  console.log("connnected");
+
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("server connected");
+});
